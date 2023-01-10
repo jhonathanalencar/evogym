5e9e07bee2930ce7759a1ca43ced1e474a3b5079
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-01-10 14:58:31 -0300

    style: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import { useEffect, useState } from 'react';
-
 export function App() {
-  const [test, setTest] = useState(false);
-
-  useEffect(() => {
-    if (!test) {
-      setTest(true);
-    }
-  }, [test]);
-
   return <div className="App">Mikasa</div>;
 }
