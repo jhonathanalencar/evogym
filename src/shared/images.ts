f08c19b57012ee01c4f4e5181397856dc5af4e5c
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-01-10 14:06:26 -0300

    feat: add assets

---
+++ src/shared/images.ts
@@ -0,0 +1,41 @@
+import AbstractWaves from '../assets/AbstractWaves.png';
+import Arrow from '../assets/Arrow.png';
+import BenefitsPageGraphic from '../assets/BenefitsPageGraphic.png';
+import Circles from '../assets/Circles.png';
+import ContactUsPageGraphic from '../assets/ContactUsPageGraphic.png';
+import EvolveText from '../assets/EvolveText.png';
+import HomePageGraphic from '../assets/HomePageGraphic.png';
+import HomePageText from '../assets/HomePageText.png';
+import Image1 from '../assets/image1.png';
+import Image2 from '../assets/image2.png';
+import Image3 from '../assets/image3.png';
+import Image4 from '../assets/image4.png';
+import Image5 from '../assets/image5.png';
+import Image6 from '../assets/image6.png';
+import Logo from '../assets/Logo.png';
+import Sparkles from '../assets/Sparkles.png';
+import SponsorForbes from '../assets/SponsorForbes.png';
+import SponsorFortune from '../assets/SponsorFortune.png';
+import SponsorRedBull from '../assets/SponsorRedBull.png';
+
+export default {
+  AbstractWaves,
+  Arrow,
+  BenefitsPageGraphic,
+  Circles,
+  ContactUsPageGraphic,
+  EvolveText,
+  HomePageGraphic,
+  HomePageText,
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Logo,
+  Sparkles,
+  SponsorForbes,
+  SponsorFortune,
+  SponsorRedBull,
+};
