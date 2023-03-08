943edd0fc6d06022e3dae3c808744664c78f8cd1
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-03-08 15:29:40 -0300

    feat: send email using emailjs library

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
+import { CircleNotch } from 'phosphor-react';
 
 import './styles.scss';
 
 interface FormData {
-  name: string;
-  email: string;
+  user_name: string;
+  user_email: string;
   message: string;
 }
 
 export function ContactForm() {
+  const [responseMessage, setResponseMessage] = useState('');
+
   const {
     handleSubmit,
     register,
@@ -16,9 +21,21 @@ export function ContactForm() {
     formState: { errors, isSubmitting, isValid },
   } = useForm<FormData>();
 
-  function sendEmail(data: FormData) {
-    console.log(data);
-    reset();
+  async function sendEmail(data: FormData) {
+    try {
+      await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        { ...data },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      );
+
+      reset();
+      setResponseMessage('Email sent successfully.');
+    } catch (error) {
+      console.log(error);
+      setResponseMessage('Something went wrong. Please try later.');
+    }
   }
 
   return (
@@ -26,38 +43,59 @@ export function ContactForm() {
       <input
         type="text"
         placeholder="name"
-        {...register('name', {
+        disabled={isSubmitting}
+        {...register('user_name', {
           required: 'Name is required',
         })}
       />
-      {errors.name ? (
-        <p className="error-message">{errors.name.message}</p>
+      {errors.user_name ? (
+        <p className="error-message" role="alert">
+          {errors.user_name.message}
+        </p>
       ) : null}
 
       <input
         type="email"
         placeholder="email"
-        {...register('email', {
+        disabled={isSubmitting}
+        {...register('user_email', {
           required: 'Email is required',
         })}
       />
-      {errors.email ? (
-        <p className="error-message">{errors.email.message}</p>
+      {errors.user_email ? (
+        <p className="error-message" role="alert">
+          {errors.user_email.message}
+        </p>
       ) : null}
 
       <textarea
+        disabled={isSubmitting}
         placeholder="message"
         {...register('message', {
           required: 'Message is required',
         })}
       />
       {errors.message ? (
-        <p className="error-message">{errors.message.message}</p>
+        <p className="error-message" role="alert">
+          {errors.message.message}
+        </p>
       ) : null}
 
       <button type="submit" disabled={isSubmitting || !isValid}>
-        submit
+        {isSubmitting ? (
+          <span className="loader">
+            <CircleNotch weight="bold" />
+          </span>
+        ) : (
+          'send'
+        )}
       </button>
+
+      {responseMessage ? (
+        <p className="contact-form__response-message" role="alert">
+          {responseMessage}
+        </p>
+      ) : null}
     </form>
   );
 }
